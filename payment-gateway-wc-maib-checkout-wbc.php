17ad90dfa31d5daf22e0b4606cfa9ef2d2fcd758
Alexander Minza <alexander.minza@gmail.com>
2026-01-01 23:11:16 +0200

    wp_set_script_translations

--- payment-gateway-wc-maib-checkout-wbc.php
+++ payment-gateway-wc-maib-checkout-wbc.php
@@ -63,7 +63,7 @@ final class WC_Gateway_MAIB_Checkout_WBC extends AbstractPaymentMethodType
         );
 
         if (function_exists('wp_set_script_translations')) {
-            wp_set_script_translations($script_id, 'payment-gateway-wc-maib-checkout', dirname(plugin_basename(__FILE__)) . '/languages');
+            wp_set_script_translations($script_id, 'payment-gateway-wc-maib-checkout', plugin_dir_path(__FILE__) . 'languages');
         }
 
         return array($script_id);
