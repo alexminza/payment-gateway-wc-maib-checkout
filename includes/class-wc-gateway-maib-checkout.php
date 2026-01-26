e6c53947cbef3e696ee0c30c45cbe3186f51f8c6
Alexander Minza <alexander.minza@gmail.com>
2026-01-26 22:57:19 +0200

    action_check_payment get_payment_gateway_instance

--- includes/class-wc-gateway-maib-checkout.php
+++ includes/class-wc-gateway-maib-checkout.php
@@ -908,6 +908,7 @@ class WC_Gateway_MAIB_Checkout extends WC_Payment_Gateway_Base
 
     public static function action_check_payment(\WC_Order $order)
     {
+        /** @var WC_Gateway_MAIB_Checkout $plugin */
         $plugin = self::get_payment_gateway_instance();
         $plugin->check_payment($order);
     }
