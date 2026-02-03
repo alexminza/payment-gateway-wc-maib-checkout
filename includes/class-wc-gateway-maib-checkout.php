3d47c2c47feb3213ce5f6cb821fb27852e44f2b6
Alexander Minza <alexander.minza@gmail.com>
2026-02-03 12:35:15 +0200

    order_actions needs_payment

--- includes/class-wc-gateway-maib-checkout.php
+++ includes/class-wc-gateway-maib-checkout.php
@@ -871,7 +871,7 @@ class WC_Gateway_MAIB_Checkout extends WC_Payment_Gateway_Base
     //region Integration
     public static function order_actions(array $actions, \WC_Order $order)
     {
-        if ($order->is_paid() || $order->get_payment_method() !== self::MOD_ID) {
+        if (!$order->needs_payment() || $order->get_payment_method() !== self::MOD_ID) {
             return $actions;
         }
 
