f6fdb779664feec77409a3b673fbb118e4523ee2
Alexander Minza <alexander.minza@gmail.com>
2026-02-16 15:26:00 +0200

    maib_checkout_payment payment_list_data count offset

--- includes/class-wc-gateway-maib-checkout.php
+++ includes/class-wc-gateway-maib-checkout.php
@@ -377,8 +377,6 @@ class WC_Gateway_MAIB_Checkout extends WC_Payment_Gateway_Base
         $payment_list_data = array(
             'orderId' => $order_id,
             'status' => 'Executed',
-            'count' => 10,
-            'offset' => 0,
         );
 
         $payment_list_response = $client->paymentList($payment_list_data, $auth_token);
